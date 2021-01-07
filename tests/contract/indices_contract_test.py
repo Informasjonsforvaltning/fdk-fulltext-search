a05472c3bc56607cecbb792f47e42ac57808ca7f
NilsOveTen <github.nils.ove@tendenes.com>
2021-01-07 09:54:09 +0100

    feat: secure /indices with api-key

--- tests/contract/indices_contract_test.py
+++ tests/contract/indices_contract_test.py
@@ -40,5 +40,10 @@ class TestSearchAll:
 
     @pytest.mark.contract
     def test_should_return_400_response(self, api):
-        result = requests.post(url=index_url + "?name=nope")
+        result = requests.post(url=index_url + "?name=nope", headers={"X-API-KEY": "test-key"})
         assert result.status_code == 400
+
+    @pytest.mark.contract
+    def test_should_return_403_response(self, api):
+        result = requests.post(url=index_url + "?name=concepts")
+        assert result.status_code == 403
